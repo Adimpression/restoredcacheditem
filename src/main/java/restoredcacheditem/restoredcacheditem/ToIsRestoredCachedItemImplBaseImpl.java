ae4fef347f41dad6eabfb8118f47a225f435fc30
Ravindranath Akila <ravindranathakila@gmail.com>
2020-05-09 12:31:16 +0800

    Working, with test cases

--- src/main/java/restoredcacheditem/restoredcacheditem/ToIsRestoredCachedItemImplBaseImpl.java
+++ src/main/java/restoredcacheditem/restoredcacheditem/ToIsRestoredCachedItemImplBaseImpl.java
@@ -1,24 +1,102 @@
 package restoredcacheditem.restoredcacheditem;
 
+import com.google.protobuf.ByteString;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ILock;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
+import restoredcacheditem.input.IsInput;
 
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class ToIsRestoredCachedItemImplBaseImpl extends ToIsRestoredCachedItemGrpc.ToIsRestoredCachedItemImplBase {
 
-    private final Logger log;
+    private final Logger logger;
+
+    private final HazelcastInstance hzInstance;
+    private final Map<String, ByteString> general;
+    private final ILock lock;
+
 
     public ToIsRestoredCachedItemImplBaseImpl() {
-        log = Logger.getLogger(getClass().getName());
-        log.info("starting");
+        logger = Logger.getLogger(getClass().getName());
+        logger.info("starting");
 
-        log.info("started");
+        logger.info("starting hazelcast");
+        hzInstance = Hazelcast.newHazelcastInstance();
+        logger.info("started hazelcast");
+
+        logger.info("starting general map: hazelcast");
+        general = hzInstance.getMap("general");
+        logger.info("started general map: hazelcast");
+
+        logger.info("starting general lock: hazelcast");
+        lock = hzInstance.getLock("general");
+        logger.info("started general lock: hazelcast");
+
+        logger.info("started");
     }
 
     @Override
     public void produce(final NotRestoredCachedItem request, final StreamObserver<IsRestoredCachedItem> responseObserver) {
-        responseObserver.onNext(IsRestoredCachedItem.newBuilder()
-                .build());
-        responseObserver.onCompleted();
+        try {
+            final IsInput isInput;
+            final String isStringValue;
+
+            if (!request.hasIsInput()) {
+                throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+            }
+
+            isInput = request.getIsInput();
+            if (!isInput
+                    .hasIsId()) {
+                throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+            }
+
+            if (!isInput.getIsId()
+                    .hasIsOutput()) {
+                throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+            }
+
+            isStringValue = isInput.getIsId()
+                    .getIsOutput()
+                    .getIsStringValue();
+
+            if (isStringValue.isEmpty()) {
+                throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+            }
+
+            if (isInput.getIsUseLockBoolean()) {
+                lock.lock();
+                try {
+                    if (general.containsKey(isStringValue)) {
+                        general.remove(isStringValue);
+                        general.put(isStringValue,
+                                isInput.getIsItemBytes());
+                    } else {
+                        throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("404"));
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                if (general.containsKey(isStringValue)) {
+                    general.remove(isStringValue);
+                    general.put(isStringValue,
+                            isInput.getIsItemBytes());
+                } else {
+                    throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("404"));
+                }
+            }
+
+            responseObserver.onNext(IsRestoredCachedItem.newBuilder()
+                    .build());
+            responseObserver.onCompleted();
+        } catch (final StatusRuntimeException e) {
+            responseObserver.onError(e);
+        }
     }
 }
--- src/test/groovy/restoredcacheditem/restoredcacheditem/ToIsRestoredCachedItemImplBaseImplTest.groovy
+++ src/test/groovy/restoredcacheditem/restoredcacheditem/ToIsRestoredCachedItemImplBaseImplTest.groovy
@@ -1,12 +1,21 @@
 package restoredcacheditem.restoredcacheditem
 
+import com.google.protobuf.ByteString
+import id.id.IsId
+import id.output.IsOutput
+import identifiedcacheditem.identifiedcacheditem.NotIdentifiedCachedItem
+import identifiedcacheditem.identifiedcacheditem.ToIsIdentifiedCachedItemGrpc
 import io.grpc.StatusRuntimeException
 import io.grpc.inprocess.InProcessChannelBuilder
 import main.Test
-import restoredcacheditem.restoredcacheditem.NotRestoredCachedItem
-import restoredcacheditem.restoredcacheditem.ToIsRestoredCachedItemGrpc
+import removedcacheditem.removedcacheditem.ToIsRemovedCachedItemGrpc
+import restoredcacheditem.input.IsInput
+import retrievedcacheditem.retrievedcacheditem.NotRetrievedCachedItem
+import retrievedcacheditem.retrievedcacheditem.ToIsRetrievedCachedItemGrpc
 import spock.lang.Shared
 import spock.lang.Specification
+import storedcacheditem.storedcacheditem.NotStoredCachedItem
+import storedcacheditem.storedcacheditem.ToIsStoredCachedItemGrpc
 
 import java.util.concurrent.TimeUnit
 
@@ -15,9 +24,25 @@ class ToIsRestoredCachedItemImplBaseImplTest extends Specification {
     @Shared
     ToIsRestoredCachedItemGrpc.ToIsRestoredCachedItemBlockingStub stub
 
+    @Shared
+    ToIsStoredCachedItemGrpc.ToIsStoredCachedItemBlockingStub toIsStoredCachedItemBlockingStub
+
+    @Shared
+    ToIsRemovedCachedItemGrpc.ToIsRemovedCachedItemBlockingStub toIsRemovedCachedItemBlockingStub
+
+    @Shared
+    ToIsRetrievedCachedItemGrpc.ToIsRetrievedCachedItemBlockingStub toIsRetrievedCachedItemBlockingStub
+
+    @Shared
+    ToIsIdentifiedCachedItemGrpc.ToIsIdentifiedCachedItemBlockingStub toIsIdentifiedCachedItemBlockingStub
+
     def setupSpec() {
         Test.before()
-        stub = ToIsRestoredCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsRestoredCachedItemGrpc.SERVICE_NAME).usePlaintext().build()).withWaitForReady()
+        toIsStoredCachedItemBlockingStub = ToIsStoredCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsStoredCachedItemGrpc.SERVICE_NAME).usePlaintext().build())
+        toIsRemovedCachedItemBlockingStub = ToIsRemovedCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsRemovedCachedItemGrpc.SERVICE_NAME).usePlaintext().build())
+        toIsRetrievedCachedItemBlockingStub = ToIsRetrievedCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsRetrievedCachedItemGrpc.SERVICE_NAME).usePlaintext().build())
+        toIsIdentifiedCachedItemBlockingStub = ToIsIdentifiedCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsIdentifiedCachedItemGrpc.SERVICE_NAME).usePlaintext().build())
+        stub = ToIsRestoredCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsRestoredCachedItemGrpc.SERVICE_NAME).usePlaintext().build()).withWaitForReady().withDeadlineAfter(2, TimeUnit.MINUTES)
     }
 
     def """Should not allow empty"""() {
@@ -28,6 +53,154 @@ class ToIsRestoredCachedItemImplBaseImplTest extends Specification {
         stub.produce(item)
 
         then:
-        thrown StatusRuntimeException
+        def exception = thrown StatusRuntimeException
+        assert exception.message == "INVALID_ARGUMENT: 422"
+
+    }
+
+    def """Should not allow empty input"""() {
+        setup:
+        def item = NotRestoredCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder().build())
+                .build()
+        when:
+        stub.produce(item)
+
+        then:
+        def exception = thrown StatusRuntimeException
+        assert exception.message == "INVALID_ARGUMENT: 422"
+    }
+
+    def """Should not allow empty input -> id"""() {
+        setup:
+        def item = NotRestoredCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder().build())
+                        .build())
+                .build()
+        when:
+        stub.produce(item)
+
+        then:
+        def exception = thrown StatusRuntimeException
+        assert exception.message == "INVALID_ARGUMENT: 422"
+    }
+
+    def """Should not allow empty input -> id -> output"""() {
+        setup:
+        def item = NotRestoredCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder().build())
+                                .build())
+                        .build())
+                .build()
+        when:
+        stub.produce(item)
+
+        then:
+        def exception = thrown StatusRuntimeException
+        assert exception.message == "INVALID_ARGUMENT: 422"
+    }
+
+    def """Should not allow empty input -> id -> output -> string value"""() {
+        setup:
+        def item = NotRestoredCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue("")
+                                        .build())
+                                .build())
+                        .build())
+                .build()
+        when:
+        stub.produce(item)
+
+        then:
+        def exception = thrown StatusRuntimeException
+        assert exception.message == "INVALID_ARGUMENT: 422"
+    }
+
+    def """Should allow replacing existing key"""() {
+        setup:
+        def key = String.valueOf(System.currentTimeMillis())
+        def item = NotRestoredCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(key)
+                                        .build())
+                                .build())
+                        .setIsUseLockBoolean(true)
+                        .setIsItemBytes(ByteString.copyFrom("New Value", "UTF-8"))
+                        .build())
+                .build()
+        def notStoredCachedItem = NotStoredCachedItem.newBuilder()
+                .setIsInput(storedcacheditem.input.IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(key)
+                                        .build())
+                                .build())
+                        .setIsUseLockBoolean(true)
+                        .setIsItemBytes(ByteString.copyFrom(key, "UTF-8"))
+                        .build())
+                .build()
+        def notRetrievedCachedItem = NotRetrievedCachedItem.newBuilder()
+                .setIsInput(retrievedcacheditem.input.IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(key)
+                                        .build())
+                                .build())
+                        .setIsUseLockBoolean(true)
+                        .build())
+                .build()
+        when:
+        toIsStoredCachedItemBlockingStub.produce(notStoredCachedItem)
+
+        and:
+        stub.produce(item)
+
+        and:
+        def isRetrievedCachedItem = toIsRetrievedCachedItemBlockingStub.produce(notRetrievedCachedItem)
+
+        then:
+        assert isRetrievedCachedItem.getIsOutput().getIsItemBytes().toString("UTF-8") == "New Value"
+    }
+
+    def """Should not allow replacing non existing key"""() {
+        setup:
+        def key = String.valueOf(System.currentTimeMillis())
+        def notIdentifiedCachedItem = NotIdentifiedCachedItem.newBuilder()
+                .setIsInput(identifiedcacheditem.input.IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(key)
+                                        .build())
+                                .build())
+                        .build())
+                .build()
+        def item = NotRestoredCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(key)
+                                        .build())
+                                .build())
+                        .setIsUseLockBoolean(true)
+                        .setIsItemBytes(ByteString.copyFrom("New Value", "UTF-8"))
+                        .build())
+                .build()
+        when:
+        assert !toIsIdentifiedCachedItemBlockingStub.produce(notIdentifiedCachedItem).getIsOutput().getIsKnownBoolean()
+
+        and:
+        stub.produce(item)
+
+        then:
+        def exception = thrown StatusRuntimeException
+        assert exception.message == "INVALID_ARGUMENT: 404"
     }
 }
